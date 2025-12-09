ea0a8a10e9a6fc66dae39385ff6d4196536655fa
moonanjum26 <moonanjum26@gmail.com>
2025-12-09 15:52:48 +0530

    Update redshift_namespace.tf

--- redshift_namespace.tf
+++ redshift_namespace.tf
@@ -1,8 +1,9 @@
 resource "aws_redshiftserverless_namespace" "redshift_namespace" {
   namespace_name = "analytics"
-  admin_username = "adminuser"
-  admin_user_password = "7adminPassword"
+  admin_username = "*******"
+  admin_user_password = "******"
   db_name = "analyticsdb"
   default_iam_role_arn = aws_iam_role.redshift_serverless_role.arn
   iam_roles = [aws_iam_role.redshift_serverless_role.arn, aws_iam_role.glue_role.arn]
+
 }
